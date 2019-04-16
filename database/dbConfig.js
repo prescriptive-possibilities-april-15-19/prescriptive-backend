f2374f405edaf998349f8a3a9444cbe995cd49e1
umbur <you@example.com>
2019-04-15 21:48:02 -0600

    Created insert test cases for db action model file

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,5 @@
-const environment = process.env.NODE_ENV || 'development';    // if nothing sets ENV, use development
-const configuration = require('../knexfile.js')[environment]; // require environment's settings from knexfile
+const environment = process.env.DB_ENV || 'development';    // if nothing sets ENV, use development
+const configuration = require('../knexfile.js')[environment] || 'development'; // require environment's settings from knexfile
 const database = require('knex')(configuration);              // connect to DB via knex using env's settings
 
 module.exports = database;
---
+++ database/migrations/20190415213429_ligands.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('ligands', ligands => {
+      ligands.increments('lig_id')
+        .primary();
+  
+      ligands.integer('PubChem CID');
+  
+      ligands.text('SMILES');
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('ligands');
+  };
+  
